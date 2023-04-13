7a0a22412fb603c46b4da058ffc485db208c5382
Donitaqueen150075 <97503052+Donitaqueen150075@users.noreply.github.com>
2023-04-13 15:35:20 +0800

    20230413

---
+++ forms/contact.php
@@ -0,0 +1,36 @@
+<?php
+
+
+  // 用您的實際接收電子郵件地址替換  
+  $receiving_email_address = 'zxc150075@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( '無法加載“ PHP電子郵件表單”庫');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+ //如果要使用SMTP發送電子郵件，請取消註釋以下代碼。 您需要輸入正確的SMTP憑據
+ 
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
